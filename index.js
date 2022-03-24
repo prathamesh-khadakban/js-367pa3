d8f537a9c1e8c1263b2ed93b5ada83f0b08d8c9d
prathamesh-khadakban <prathamesh.khadakban@gmail.com>
2022-03-24 09:26:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+class Data {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-//
+  getName() {
+    console.log(`This is ${this.name}, and he is ${this.age} months old`);
+  }
+}
+
+let car = new Data('shourya', '4');
+car.getName();
